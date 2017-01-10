3a2fb604d71519c3e380dc93a2139e8758e334b2
notfound47 <compnerds@gmail.com>
2017-01-09 22:54:18 -0800

    WebsiteOptimization Commit
    Committing source code of Website Optimization

---
+++ Gruntfile.js
@@ -0,0 +1,203 @@
+
+/*
+ After you have changed the settings at "Your code goes here",
+ run this with one of these options:
+ "grunt" alone creates a new, completed images directory
+ "grunt clean" removes the images directory
+ "grunt responsive_images" re-processes images without removing the old ones
+ */
+
+module.exports = function(grunt) {
+
+    grunt.initConfig({
+
+        htmlmin: {
+            target: {
+                options: {
+                    removeComments: true,
+                    collapseWhitespace: true
+                },
+                files: {
+                    'dist/index.html': 'index.html',
+                    'dist/views/pizza.html': 'views/pizza.html'
+                }
+            }
+        },
+        cssmin: {
+            target: {
+                files: [{
+                    expand: true,
+                    cwd: 'css/',
+                    src: ['*.css', '!*.min.css'],
+                    dest: 'dist/css/',
+                    ext: '.min.css'
+                }, {
+                    expand: true,
+                    cwd: 'views/css/',
+                    src: ['*.css', '!*.min.css'],
+                    dest: 'dist/views/css/',
+                    ext: '.min.css'
+                }]
+            }
+        },
+        uglify: {
+            options: {
+                mangle: false,
+                compress: {
+                    drop_console: false,
+                    dead_code: true,
+                    unused: true
+                }
+            },
+
+            my_target: {
+                files: [{
+                    expand: true,
+                    cwd: 'js/',
+                    src: '*.js',
+                    dest: 'dist/js/',
+                    ext: '.min.js'
+                }, {
+                    expand: true,
+                    cwd: 'views/js/',
+                    src: '*.js',
+                    dest: 'dist/views/js/',
+                    ext: '.min.js'
+                }]
+            }
+
+        },
+        responsive_images: {
+            resize: {
+                options: {
+                    engine: 'im',
+                    sizes: [{
+                        width: 400,
+                        quality: 50
+                    },
+                    {
+                        width: 800,
+                        quality: 50
+                    }]
+                },
+
+                /*
+                 You don't need to change this part if you don't change
+                 the directory structure.
+                 */
+                files: [{
+                    expand: true,
+                    src: ['*.{gif,jpg,png,jpeg}'],
+                    cwd: 'img/',
+                    dest: 'dist/img/'
+                }, {
+                    expand: true,
+                    src: ['*.{gif,jpg,png,jpeg}'],
+                    cwd: 'views/images',
+                    dest: 'dist/views/images/'
+                }]
+            }
+        },
+
+        /* Clear out the images directory if it exists */
+        clean: {
+            all: {
+                src: ['dist', 'views/dist'],
+            },
+            dist: {
+                src: ['views/dist'],
+            },
+        },
+
+
+        /* Generate the images directory if it is missing */
+        mkdir: {
+            dev: {
+                options: {
+                    create: ['dist/img', 'dist/css', 'views/dist/css', 'dist/js', 'views/dist/js']
+                }
+            }
+        },
+
+        compress: {
+            main: {
+                options: {
+                    mode: 'gzip'
+                },
+                files: [{
+                    expand: true,
+                    cwd: 'dist/',
+                    src: ['index.html', 'js/analytics.min.js'],
+                    dest: '/'
+                }]
+            }
+        },
+
+        /* Copy the "fixed" images that don't go through processing into the images/directory */
+        copy: {
+            dev: {
+                files: [{
+                    cwd: 'img/',
+                    src: '*.{gif,jpg,png}',
+                    dest: 'dist/img/',
+                    expand: true
+                }, {
+                    cwd: 'views/images/',
+                    src: '*.{gif,jpg,png}',
+                    dest: 'dist/views/images/',
+                    expand: true
+                }, {
+                    cwd: 'views/dist/',
+                    src: ['js/*.*', 'css/*.*', '*.html'],
+                    dest: 'dist/views/',
+                    expand: true
+                }]
+            }
+        },
+
+        cache_control:   {
+            my_target:   {
+                source:  "dist/index.html",
+                options:   {
+                    version:  "1.0",
+                    links:  true,
+                    scripts:  true,
+                    images: true,
+                    replace:  true,
+                    ignoreCDN:  true,
+                    dojoCacheBust:  true
+                }
+            }
+        },
+
+        replace:   {
+            my_target:   {
+                src:   ['dist/index.html', 'dist/views/pizza.html'],
+                overwrite:  true,
+                 
+                replacements:   [{
+                    from:  '.js',
+                     
+                    to:  '.min.js'
+                }, {
+                    from:  '.css',
+                    to:  '.min.css'
+                }, {
+                    from:  '.jpg',
+                    to:  '-800.jpg'
+                }]
+            }
+        }
+    });
+    grunt.loadNpmTasks('grunt-contrib-cssmin');
+    grunt.loadNpmTasks('grunt-contrib-uglify');
+    grunt.loadNpmTasks('grunt-responsive-images');
+    grunt.loadNpmTasks('grunt-contrib-copy');
+    grunt.loadNpmTasks('grunt-contrib-clean');
+    grunt.loadNpmTasks('grunt-contrib-htmlmin');
+    grunt.loadNpmTasks('grunt-contrib-compress');
+    grunt.loadNpmTasks('grunt-cache-control');
+    grunt.loadNpmTasks('grunt-text-replace');
+    grunt.loadNpmTasks('grunt-mkdir');
+    grunt.registerTask('default', ['clean:all', 'mkdir', 'htmlmin', 'cssmin', 'uglify', 'responsive_images', 'copy', 'compress', 'cache_control', 'replace']);
+}; 
---
+++ README.md
@@ -0,0 +1,119 @@
+# Website Performance Optimization
+Optimized an online portfolio for speed--in particular--optimized the critical rendering path and made this page render as quickly as possible by applying the techniques from the [Critical Rendering Path course](https://www.udacity.com/course/ud884).
+
+####Objective 1: Optimize PageSpeed Insights score for index.html
+
+This score can be determined after first creating a simple Python server:
+
+  ```bash
+  cd /path/to/your-project-folder
+  python -m SimpleHTTPServer 8080
+  ```
+
+Followed by hosting using [ngrok](https://ngrok.com/) to make the server accesible remotely.
+
+  ``` bash
+  cd /path/to/your-project-folder
+  ngrok http 8080
+  ```
+
+Finally, the score can be determined using [Google PageSpeed Insights](https://developers.google.com/speed/pagespeed/insights/).
+
+####Objective 2: Optimize Frames per Second in pizza.html
+
+To optimize views/pizza.html, you will need to modify views/js/main.js until your frames per second rate is 60 fps or higher. You will find instructive comments in main.js.
+
+##Modifications
+
+####Modification 1: Minify JavaScript and load asynchronously
+
+Minification can be easily performed using Grunt or other task runners.  Using [Grunt](http://gruntjs.com/) you are able to to minify the JS. I added "async" to the script tag in the HTML, which prevents the JS from render blocking, reducing the critical path length.
+
+####Modification 2: Compress images
+
+Loading large images can be a strain on the browser. Using Grunt with ImageMagick allows you to optimize the image for responsiveness and also replaced the filename in code with the optimzed images generated. 
+
+####Modification 3: Selectively load CSS
+
+The browser does not need to load CSS for every situation, rather, HTML can dictate when it is necessary for it to load. Using the media setting in the link tag allowed me to block a "print-only" CSS file from constantly loading.
+
+  ```html
+  <link "css/print.css" rel="stylesheet" media="print">
+  ```
+
+####Modification 4: Use a web font loader for custom fonts
+
+Custom fonts, such as those found at [Google Fonts](https://www.google.com/fonts), can be optimized using a web font loading script.  I placed the code at the end of the browser, to increase the [PageSpeed Score](https://developers.google.com/speed/pagespeed/insights/):
+
+  ```javascript
+  <script>
+    WebFontConfig = {
+      google: {
+        families: ['Open Sans:400,700']
+      }
+    };
+
+    (function(w,g){w['GoogleAnalyticsObject']=g;w[g]=w[g]||function(){(w[g].q=w[g].q||[]).push(arguments)};w[g].l=1*new Date();})(window,'ga');
+
+    // Optional TODO: replace with your Google Analytics profile ID.
+    ga('create', 'UA-XXXX-Y');
+    ga('send', 'pageview');
+
+     (function(d) {
+        var wf = d.createElement('script'), s = d.scripts[0];
+        wf.src = 'https://ajax.googleapis.com/ajax/libs/webfont/1.5.18/webfont.js';
+        s.parentNode.insertBefore(wf, s);
+     })(document);
+  </script>
+  ```
+
+More information can be found at the [Web Font Loader Github Repo](https://github.com/typekit/webfontloader).
+
+####Modification 5: Inline CSS in production code
+
+While inlining CSS is bad practice for source code, it can be safely inlined in production code to improve performance. I used the Grunt to optimize code after implementing the inlined CSS code.
+
+####Modification 6: Altering the pizza.html page
+
+The main.js code provides the page with moving pizzas which has trouble rendering at 60 fps. This is because of how the loops are constructed, they require the DOM to be fetched at each iteration.
+
+Additionally, the "querySelectorAll" does not provide as narrow of a selector as "getElementsByClassName", which increases time the browser requires to find the DOM element. For example, the unaltered code followed by the altered code from the for-loop.
+
+######Before:
+
+  ```javascript
+  var items = document.querySelectorAll('.mover');
+  for (var i = 0; i < items.length; i++) {
+    var phase = Math.sin((document.body.scrollTop / 1250) + (i % 5));
+    items[i].style.left = items[i].basicLeft + 100 * phase + 'px';
+  }
+  ```
+
+######After:
+
+  ```javascript
+  var items = document.getElementsByClassName("mover");
+
+  // Caching variables to improve performance
+  var scrollTopLookup = document.body.scrollTop;
+  var constArray = [];
+
+  // Optimized code that takes less effort to go through the phases
+  // and calculate the scrollTopLookup variable
+  for (i = 0; i < 5; i++) {
+    constArray.push(Math.sin((scrollTopLookup / 1250) + i));
+  }
+
+  // Utilizes the previous for-loop's ability to generate values for
+  // the phases to reduce the need to lookup values
+  for (i = 0; i < items.length; i++) {
+    var phase = constArray[i % 5];
+    items[i].style.left = items[i].basicLeft + 100 * phase + 'px';
+  }
+  ```
+
+The [utilized code](https://discussions.udacity.com/t/project-4-how-do-i-optimize-the-background-pizzas-for-loop/36302) was provided by mcs in the discussion forum as a hint to help optimize the page. Thanks!
+
+###About
+
+This project was part of Udacity's [Front-End Web Developer Nanodegree](https://www.udacity.com/course/front-end-web-developer-nanodegree--nd001), with the goal to optimize a website above a PageSpeed of 90 and to acheive 60 fps.
